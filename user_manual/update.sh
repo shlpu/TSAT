e5a841204cc63824d6e7cceef537ea667b9f3d5b
Drew Wicke <drewwicke@gmail.com>
2017-07-07 09:01:53 -0700

    fixed copy

--- user_manual/update.sh
+++ user_manual/update.sh
@@ -7,4 +7,4 @@ cp tsat-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Desktop/TSAT.jar
 cd ..
 cd user_manual
 cp User\ Manual.pdf ~/Desktop/
-cp datasets ~/Desktop/datasets
+cp -r datasets ~/Desktop/datasets
