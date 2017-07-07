42b27bb446be71456f12daf9f22b16ab7787724d
Drew Wicke <drewwicke@gmail.com>
2017-07-07 08:56:36 -0700

    created a script for easy updating

---
+++ user_manual/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/Documents/src/TSAT/
+git pull
+mvn package -Psingle
+cd target
+chmod +x tsat-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+cp tsat-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Desktop/TSAT.jar
+cd ..
+cd user_manual
+cp User\ Manual.pdf ~/Desktop/
+cp datasets ~/Desktop/datasets
